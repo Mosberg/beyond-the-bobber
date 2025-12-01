2079bdc5d21506fc4ac48ef97d9dd1b8a7253c3d
Mosberg <RasmusTMosberg@gmail.com>
2025-12-01 04:23:00 +0100

    Update BeyondTheBobber mod main class to register items

--- src/main/java/com/mosberg/BeyondTheBobber.java
+++ src/main/java/com/mosberg/BeyondTheBobber.java
@@ -1,7 +1,7 @@
 package com.mosberg;
 
+import com.mosberg.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,10 +15,7 @@ public class BeyondTheBobber implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerItems();
+		LOGGER.info("Beyond the Bobber mod initialized with custom fish item.");
 	}
 }
